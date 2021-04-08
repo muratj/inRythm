825bbdda1e606ddd3f58792368dfa7a40b595f69
murat <muratj.tm@gmail.com>
2021-04-08 18:04:18 -0400

    created methods: pageCount, itemCount, pageItemCount,pageIndex

--- src/main/java/utils/PaginationHelper.java
+++ src/main/java/utils/PaginationHelper.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PaginationHelper {
     /**
@@ -22,9 +23,72 @@ public class PaginationHelper {
         this.items = createPages(items, itemsPerPage);
         this.itemsPerPage = itemsPerPage;
     }
+    public PaginationHelper(char[] items, int itemsPerPage) {
+        this.items = createPages(items, itemsPerPage);
+        this.itemsPerPage = itemsPerPage;
+    }
+    public PaginationHelper(String[] items, int itemsPerPage) {
+        this.items = createPages(items, itemsPerPage);
+        this.itemsPerPage = itemsPerPage;
+    }
+
 
     /**
-     * Method will create List of List items
+     * Gives how many pages in the pagination
+     * @return pages size
+     */
+    public int pageCount() {
+        return items.size();
+    }
+
+    /**
+     * Returns how many items in the pagination
+     * @return count of items
+     */
+    public int itemCount() {
+        AtomicInteger count = new AtomicInteger();
+        items.forEach(page -> {
+            page.forEach(item -> {
+                count.getAndIncrement();
+            });
+        });
+        return count.get();
+    }
+
+    /**
+     * Returns how many items in the provided page
+     * @param pageIndex index of the page
+     * @return items count in the page
+     */
+    public int pageItemCount(int pageIndex) {
+        if (pageIndex >= items.size()) return -1;
+        return items.get(pageIndex).size();
+    }
+
+    /**
+     * Returns page index where located the item provided as a parameters
+     * @param itemIndex index of the item based on initial array
+     * @return page index starts from 0
+     */
+    public int pageIndex(int itemIndex) {
+        if (itemIndex >= itemCount()) return -1;
+        int pageNumber = 0;
+        int endIndex = 0;
+
+        for (int i=0; i<items.size(); i++) {
+            int startIndex = i*itemsPerPage;
+            endIndex += itemsPerPage-1;
+
+            if(itemIndex>=startIndex && itemIndex<=endIndex){
+                pageNumber = i;
+            }
+        }
+
+        return pageNumber;
+    }
+
+    /**
+     * Method will create List of List items from int[]
      * @param items require parameter int array
      * @param itemsPerPage how many Items per page
      * @return List of List items
@@ -49,7 +113,63 @@ public class PaginationHelper {
 
         return pagination;
     }
+    /**
+     * Method will create List of List items from char[]
+     * @param items require parameter int array
+     * @param itemsPerPage how many Items per page
+     * @return List of List items
+     */
+    public List<List<Object>> createPages(char[] items, int itemsPerPage) {
+        int pages = (int) Math.ceil((double) items.length / itemsPerPage);
+        List<List<Object>> pagination = new ArrayList<>();
 
+        for (int i = 0; i < pages; i++) {
+            int startIndex = i * itemsPerPage;
+            int length = Math.min(items.length-startIndex, itemsPerPage);
+
+            List<Object> page = new ArrayList<>();
+            char[] temp = new char[length];
+            System.arraycopy(items, startIndex, temp, 0, length);
+            for (Object c : temp) {
+                page.add(c);
+            }
+
+            pagination.add(page);
+        }
+
+        return pagination;
+    }
+    /**
+     * Method will create List of List items from char[]
+     * @param items require parameter int array
+     * @param itemsPerPage how many Items per page
+     * @return List of List items
+     */
+    public List<List<Object>> createPages(String[] items, int itemsPerPage) {
+        int pages = (int) Math.ceil((double) items.length / itemsPerPage);
+        List<List<Object>> pagination = new ArrayList<>();
+
+        for (int i = 0; i < pages; i++) {
+            int startIndex = i * itemsPerPage;
+            int length = Math.min(items.length-startIndex, itemsPerPage);
+
+            List<Object> page = new ArrayList<>();
+            String[] temp = new String[length];
+            System.arraycopy(items, startIndex, temp, 0, length);
+            for (Object c : temp) {
+                page.add(c);
+            }
+
+            pagination.add(page);
+        }
+
+        return pagination;
+    }
+
+    /**
+     * getter for List<List<Object>> items
+     * @return List<List<Object>> items
+     */
     public Object getItems() {
         return items;
     }
--- src/test/java/unitTests/PaginationTest.java
+++ src/test/java/unitTests/PaginationTest.java
@@ -1,6 +1,8 @@
 package unitTests;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
 import utils.PaginationHelper;
 
 import java.util.Arrays;
@@ -8,16 +10,50 @@ import java.util.Collections;
 import java.util.List;
 
 public class PaginationTest {
+    // Test Data
+    char[] charArr = {'a','b','c','d','e','f'};
+    int[] intArr = {1,2,3,4,5,6,3,2,1};
+    String[] strArr = {"a", "b", "c", "d","e","f"};
+
+
+    PaginationHelper helper;
     @Test
     public void classVerification() {
-//        char[] arr = {'1','2','3','4','5','6'};
-        int[] arr = {1,2,3,4,5,6};
-        PaginationHelper helper = new PaginationHelper(arr, 4 );
+        PaginationHelper helper = new PaginationHelper(charArr, 4 );
 
-        System.out.println(helper.getItems());
+        Assert.assertEquals(helper.getItems().toString(), "[[a, b, c, d], [e, f]]");
+    }
+
+    @Test
+    public void pagesTest() {
+        helper = new PaginationHelper(intArr, 4);
+
+        Assert.assertEquals(helper.pageCount(), 2);
+    }
+
+    @Test
+    public void itemsCount() {
+        helper = new PaginationHelper(intArr, 4);
+
+        System.out.println(helper.itemCount());
+    }
+
+    @Test
+    public void itemsPerPage() {
+        helper = new PaginationHelper(strArr, 4);
+
+        System.out.println(helper.pageItemCount(3));
+    }
+
+    @Test
+    public void findPageByItemIndex() {
+        helper = new PaginationHelper(strArr, 4);
+        System.out.println(helper.pageIndex(8));
     }
 
     @Test
     public void test1() {
+        Object arr = new int[]{2,3,4,4};
+        System.out.println(Arrays.toString((int[]) arr));
     }
 }
